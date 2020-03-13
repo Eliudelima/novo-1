b3881b135db762009ad2d8de58c848368bcf856e
Eliudelima <eliudeangelo@gmail.com>
2020-03-13 19:21:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# novo-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/novo-1)
---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Gospel',
+    cantor: 'Ana Paula Valadão',
+      },
+  {
+    name: 'Samba',
+    cantor: 'Fundo de Quintal',
+     },
+  {
+    name: 'Rock',
+    cantor: 'Pink Floyd',
+    },
+{
+  name: 'Eletrônica',
+  cantor: 'DJ PV',
+},
+{
+  name:'Internacional',
+  cantor: 'Hillsong United',
+}
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
